e4ed468c1ff8866f2e9e42a43c9e98de83940fd8
vvkquau <ducnhan141106@gmail.com>
2022-11-18 01:27:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function phone() {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText("0948107722");
+
+    /* Alert the copied text */
+    alert("You have copied my phone  number ❤");
+}
+
+function email() {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText("lequynhhuong7979@gmail.com");
+
+    /* Alert the copied text */
+    alert("You have copied my email address ❤");
+}
+
+function bank() {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText("2050201201222");
+
+    /* Alert the copied text */
+    alert("You have copied my bank number account ❤");
+}
